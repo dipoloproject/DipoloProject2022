5ac99f0e2315e766b6efbe2cb3468e1d5b7222a7
Gonzalo_GAP <gonza_it@hotmail.com>
2022-02-07 23:41:50 -0300

    commit creacion del archivo index.php en la carpeta admin (contendra las rutas)

---
+++ DIPOLO_MVC/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once __DIR__ . '/../includes/app.php';
+
+    /*En el router se registran todas las url que soporta el proyecto y qué controlador
+    con qué método se va a encargar de crear, modificar, listar y eliminar */
+
+    use MVC\Router;
+
+    $router= new Router();
+
+
+?>
