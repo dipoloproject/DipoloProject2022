f64756dceed3a3010f247ba2421aa47f6ea5a0ab
IvanFurius <iav.alexis.ivan@gmail.com>
2022-02-07 23:20:26 -0300

    commit de archivos creados

---
+++ DIPOLO_MVC/admin/lista-roles.php
@@ -0,0 +1,148 @@
+<?php
+
+        include_once 'funciones/sesiones.php';/*contiene las funciones que controlan:
+                                                      si el usuario esta logueado
+                                                      si tiene permitido visitar la pagina/archivo actual*/
+
+    //include_once 'funciones/funciones.php';
+    require_once ('../includes/app.php'); 
+    use Model\Rubro;
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+        /* Eliminacion del Rubro seleccionado */                                    //
+                                                                                    //
+    /*  VALIDAR $_POST['id-registro'] que contiene el idRubro del rubro a elinminar //
+    Al ingresar a esta pagina, se evalua el POST que se recibe,                     //
+    si el valor NO es entero, se redirecciona a /admin/lista-rubros.php*/           //
+    if( isset($_POST['id-registro']) ) {                                            //
+      $id= filter_var($_POST['id-registro'], FILTER_VALIDATE_INT);                  //
+      if($id) {                                                                     //
+        //$rubro= Rubro::find($id);                                                   //
+        //$rubro->eliminar();                                                         //
+      }                                                                             //
+    }                                                                               //
+//////////////////////////////////////////////////////////////////////////////////////
+
+        include_once 'templates/header.php';
+        include_once 'templates/barra.php';
+        include_once 'templates/navegacion.php';
+        
+
+
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Listado de Roles
+        <!-- <small></small> -->
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box box-danger">
+            <div> <!-- class="box-header"-->
+              <!-- <h3 class="box-title">Sección de administración de Productos</h3> -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="registros" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>Nombre</th>
+                  <!-- <th>Precio</th> -->
+                  <!-- <th>Descripción</th> -->
+                  <th>Acciones</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        /*try {
+
+                          $productos = Producto::all(); //la variable guarda un arreglo de objetos (registros o filas del resultSet)
+
+
+                            // $sql = "SELECT idCategoria, idProducto, nombreProducto, precio, peso, color, descripcion FROM Productos";
+                            // $resultado = $db->query($sql);
+                        } catch (Exception $e) {
+                            $error = $e->getMessage();
+                            echo $error;
+                        }*/
+                        $roles = Rubro::allTopLevel(); //la variable guarda un arreglo de objetos (registros o filas del resultSet)
+                        //debuguear($productos);
+                        foreach( $rubros as $rubro ): ?>
+                        <?php if($rubro->idRubroPadre == null ) {?>
+                            <tr>
+                                <td> <?php echo $rubro->nombreRubro; ?> </td>
+                                <!-- <td>$&nbsp<?php echo $producto->precioVentaProducto; ?> </td> -->
+                                <!-- <td> <?php echo $producto->descripcionProducto; ?> </td> -->
+                                
+                                <!-- Botones de ACCIONES: Crear/Editar/Eliminar -->
+                                <!-- La accion Eliminar será un formulario, las demas seran enlaces -->
+                                <td style="display: inline-block;" class="box-create-info-edit-delete">
+                                    <a  href="crear-subrubro.php?id=<?php echo $rubro->idRubro; ?>" 
+                                        class="btn btn-xs bg-green btn-flat margin"
+                                        title="Crear subrubro">
+                                            <i class="fa fa-sitemap" ></i>
+                                    </a>
+                                    <a  href="lista-subrubros.php?id=<?php echo $rubro->idRubro; ?>" 
+                                        class="btn btn-xs bg-orange btn-flat margin"
+                                        title="Mostrar subrubros">
+                                            <i class="fa fa-info-circle" ></i>
+                                    </a>
+                                    <a  href="actualizar-rubro.php?id=<?php echo $rubro->idRubro; ?>" 
+                                        class="btn btn-xs bg-blue btn-flat margin"
+                                        title="Editar">
+                                            <i class="fa fa-pencil" ></i>
+                                    </a>
+                                   
+                                    <form role="form" class="form_eliminar-rubro btn btn-xs bg-red btn-flat margin" name="form_eliminar-rubro" method="post" action="action-rubro.php">
+                                      <button type="submit" class="btn btn-xs bg-red btn-flat" title="Eliminar" style="padding: 0rem;">
+                                        <i class="fa fa-trash" ></i>
+                                      </button>
+                                      <input type="hidden" name="button-action-rubro" value="eliminar"><!-- $_POST['crear-producto] indicara en el archivo de action si éste fue presionado-->
+                                      <input type="hidden" name="id-registro" value="<?php echo $rubro->idRubro; ?>">
+                                    </form>
+                                </td>
+                            </tr>
+
+
+                        <?php } ?>    
+                        <?php endforeach; ?>
+
+                
+                </tbody>
+                <tfoot>
+                <tr>
+                    <th>Nombre</th>
+                    <!-- <th>Precio</th> -->
+                    <!-- <th>Descripción</th> -->
+                    <th>Acciones</th>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+
+
+  </div>
+  <!-- /.content-wrapper -->
+
+<?php
+      include_once 'templates/footer.php';
+?>
+
---
+++ DIPOLO_MVC/models/rol.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Model;
+
+class Rol extends ActiveRecord {
+    
+    protected static $tabla= 'Rol';
+    protected static $columnasDB = [    'idRol', 
+                                        'nombreRol', 'estadoRol',
+                                        'observacionesRol'];
+    
+//  Se definen los atributos public (que serán las columnas de la tabla Rubros)
+    public $idRol;
+    public $nombreRol;
+    public $estadoRol;
+    public $observacionesRol; 
+
+    public function __construct($args=[])
+    {
+        // La inicializacion de idRubro puede OMITIRSE
+        if( isset($args['idRol']) ) {$this->idRol = intval($args['idRol']);}
+        else {$this->idRol = 0;}
+        //isset($args['idRubro']) ? $this->idRubro = intval($args['idRubro']) : $this->idRubro = 0;
+
+        $this->nombreRol = $args['nombreRol'] ?? '' ;
+        $this->estadoRol = $args['estadoRol'] ?? '' ;
+        $this->observacionesRol = $args['observacionesRol'] ?? '' ;
+        
+        /*if( isset($args['esPadre']) ) {$this->esPadre = intval($args['esPadre']);}
+        else {$this->esPadre = 'N';}*/ //se piensa que NO hace falta inicializar este atributo
+
+    }//los campos de la instancia creada tendra valores que se le pasan en el array o tendrán ''
+
+
+    public  function guardar() {
+        // Para la INSERCION se utilizara prepare, bind_param y execute
+
+        /*Hay 2 alternativas:
+                                El rubro a crear NO tenga rubro padre
+                                El rubro a crear sea un rubro hijo (tenga un rubro padre)*/
+        
+        $stmt = self::$db->prepare( "INSERT INTO    `Roles` (  nombreRol, estadoRol,
+                                                                observacionesRol) 
+                                                VALUES ( ?, ?, ?) ;" );
+
+        $stmt->bind_param("sss",        $this->nombreRol, $this->estadoRol,
+                                        $this->observacionesRol);
+        $stmt->execute();
+                    
+        if($stmt->affected_rows>0) {
+                //echo "La insercion fue exitosa";
+            return true;
+        } else 
+            {
+                //echo "NO se pudo insertar";
+                return false;
+            }
+            $stmt->close();
+            self::$db->close();
+              
+        //Se CIERRAN el StateMent y la conexion a la base de datos de esta instancia
+         //$stmt->close();
+        // self::$db->close();
+    }// FIN guardar()
+
+
+    /*public function actualizar() {
+        // echo "<pre>";
+        // var_dump($this->nombreRubro);
+        // var_dump($this->descripcionRubro);
+        // var_dump($this->ordenRubro);
+        // var_dump($this->destacadoRubro);
+        // var_dump($this->menuRubro);
+        // var_dump($this->estadoRubro);
+        // var_dump($this->idRubro);
+        // echo "</pre>";
+
+        $stmt = self::$db->prepare( "UPDATE `Rubros` SET    nombreRubro= ?, descripcionRubro= ?,
+                                                            ordenRubro= ?, destacadoRubro= ?,
+                                                            menuRubro= ?, estadoRubro= ?
+                                        WHERE   idRubro= ? ;" );
+
+        $stmt->bind_param("ssisssi",    $this->nombreRubro, $this->descripcionRubro,
+                                        $this->ordenRubro, $this->destacadoRubro, 
+                                        $this->menuRubro, $this->estadoRubro, 
+                                        $this->idRubro );
+
+        /*$stmt = self::$db->prepare( "INSERT INTO    `Rubros` (  idRubroPadre, 
+                                                                nombreRubro, descripcionRubro,
+                                                                ordenRubro, destacadoRubro,
+                                                                menuRubro, estadoRubro) 
+                        VALUES ( ?, ?, ?, ?, ?, ?, ?) ;" );
+
+        $stmt->bind_param("ississs",    $this->idRubroPadre, 
+                                        $this->nombreRubro, $this->descripcionRubro,
+                                        $this->ordenRubro, $this->destacadoRubro, 
+                                        $this->menuRubro, $this->estadoRubro);*/
+
+
+        /*$stmt = self::$db->prepare( "UPDATE Rubros SET nombreRubro= ? WHERE idRubro= ?;" );
+
+        $stmt->bind_param("si", $this->nombreRubro, $this->idRubro );
+
+
+
+
+
+        //debuguear($stmt);
+        $stmt->execute();
+
+        return true;
+        // debuguear($stmt);
+
+        //$stmt->affected_rows siempre devolvera 0, ESTO OCURRE CON UPDATE y supuestamente con DELETE
+        if($stmt->affected_rows>0) {
+            //echo "La actualizacion fue exitosa";
+            return true;
+        } else {
+            //echo "NO se pudo actualizar";
+            //return false;
+            return true;
+        }
+
+        $stmt->close();
+        self::$db->close(); 
+
+        //echo "LUEGO DEL EXECUTE DE ACTUALIZAR";
+        //echo $query;
+        //debuguear($resultado);
+    }//FIN actualizar()
+
+    public function eliminar() {
+        
+        $stmt = self::$db->prepare( "DELETE FROM `Rubros` WHERE idRubro= ? LIMIT 1 ;" );
+        $stmt->bind_param("i",    $this->idRubro );
+        $stmt->execute();
+
+        if($stmt->affected_rows>0) {
+            //echo "La eliminacion fue exitosa";
+            return true;
+        } else {
+            //echo "NO se pudo eliminar";
+            return false;
+        }
+
+        $stmt->close();
+        self::$db->close(); 
+    }//FIN eliminar()
+
+*/
+
+
+
+
+    //Listar todos los productos
+    public static function all() {
+        
+        $query= "SELECT * FROM `Roles`; ";
+        $resultado= self::consultarSQL($query);
+        //debuguear("antes del return");
+        //debuguear($resultado);
+        return $resultado;
+    }
+
+    //Busca un registro por idProducto
+    public static function find($id) {
+        $query = "SELECT * FROM " . static::$tabla . " WHERE " . static::$columnasDB[0] . " = ${id}";
+
+
+        //debuguear($query);
+
+        $resultado = self::consultarSQL($query);//$resultado guarda un vector con un objeto como elemento
+        return array_shift($resultado);//retorna el elemento de la primera posicion del vector guardado en $resultado
+    }
+
+
+    public static function consultarSQL($query)
+    {
+        //Consultar la BD con el query
+        $resultado= self::$db->query($query);
+        //Iterar los resultados (para obtenerlos a todos y no solo al ultimo que se trae)
+              
+        $array= [];
+        while( $registro = $resultado->fetch_assoc() ) {
+            $array[]= self::crearObjeto($registro); 
+        }
+        //Liberar la memoria
+        $resultado->free();
+        //retornar los resultados
+        return $array;
+    }
+
+    /*  Toma cada arreglo de la funcion query() para crear objetos*/
+    public static function crearObjeto($registro) {
+        $objeto= new self;
+
+        foreach($registro as $key => $value) {
+            if( property_exists( $objeto, $key ) ) {
+                $objeto->$key = $value;
+            }
+        }
+        return $objeto;
+    }
+
+/*  Esta funcion empata/mapea los atributos del objeto $producto con las llaves
+    del arreglo args[] */
+    public function sincronizar($args=[]) {
+        foreach($args as $key => $value) {
+            if( property_exists($this, $key) && !is_null($value)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+}// FIN CLASE  Rubros
+
+
+
+
+?>
